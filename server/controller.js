15ab68aff643ba1fa4b82138a00268d6dae90810
k1r0s <ciroreed@gmail.com>
2018-01-13 16:01:42 +0100

    basic setup

--- server/controller.js
+++ server/controller.js
@@ -1,55 +1,23 @@
-const { extend, inject, override } = require("kaop");
-const AbstractResourceController = require("./abstract-resource-controller");
-const { nodeProvider } = require("./node-server");
-const { ormProvider } = require("./orm-adapter");
+const { extend, override } = require("kaop");
+const AbstractResourceController = require("../lib/abstract-resource-controller");
 const { Log } = require("./logger");
 
 module.exports = Controller = extend(AbstractResourceController, {
-
-  nodeInstance: null,
-  ormInstance: null,
-
-  constructor: [
-    override.implement,
-    inject.args(nodeProvider, ormProvider),
-    function(superClass, resourceIdentifier, node, orm){
-      if(!resourceIdentifier) throw new Error("one argument is required as resource path")
-      superClass(resourceIdentifier, node);
-      this.nodeInstance = node;
-      this.ormInstance = orm;
-    }
-  ],
-  get: [Log, function() {}],
-  push: [Log, function() {}],
-  put: [Log, function() {}],
-  delete: [Log, function() {}],
+  get: [Log, function(req, res) {
+    res.statusCode = 200;
+    res.end();
+  }],
+  push: [Log, function(req, res) {
+    res.statusCode = 200;
+    res.end();
+  }],
+  put: [Log, function(req, res) {
+    res.statusCode = 200;
+    res.end();
+  }],
+  delete: [Log, function(req, res) {
+    res.statusCode = 200;
+    res.end();
+  }],
   options() {}
 });
-
-/*
-function writeResult(){
-    var res = meta.args[1];
-    if(meta.result.then){
-        meta.result.then(function(result){
-            res.statusCode = 200;
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.write(JSON.stringify(result));
-            res.end();
-        });
-        meta.result.catch(function(result){
-            console.log(result);
-            res.statusCode = 500;
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.write(JSON.stringify({ message: "there was an error :(" }));
-            res.end();
-        });
-    }else{
-        var result = meta.result;
-        res.statusCode = result.status;
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.write(JSON.stringify(result.body));
-        res.end();
-    }
-},
-
-*/
--- server/main.js
+++ server/main.js
@@ -1,3 +1,5 @@
 const Controller = require("./controller");
 
-new Controller("/person");
+new Controller("/resource1");
+new Controller("/resource2");
+new Controller("/resource3");
